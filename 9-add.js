bc8db46833524101064a7df5f5cb4750a46f648e
Reda <redamgharni18@gmail.com>
2025-05-25 21:35:26 +0100

    last one

---
+++ 9-add.js
@@ -0,0 +1,11 @@
+// script.js
+
+function add(a, b) {
+  return a + b;
+}
+
+const args = process.argv.slice(2);
+const num1 = Number(args[0]);
+const num2 = Number(args[1]);
+
+console.log(add(num1, num2));
